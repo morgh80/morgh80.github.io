c09f33baee0de820f32b139b80b62829d2d0a961
Aeronaut <aeronaut@MacBook-Pro-Aeronaut-2.local>
2017-11-13 20:13:38 +0100

    test

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ $(document).ready(function() {
     var requestUrl = apiRoot + 'deleteTask';
 
     $.ajax({
-      url: requestUrl + '/?' + $.param({
+      url: requestUrl + '?' + $.param({
         taskId: taskId
       }),
       method: 'DELETE',
